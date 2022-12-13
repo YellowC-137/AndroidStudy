4f0a11da1ba03d5ab7fb1cbac29d3fd85b05f809
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2022-12-13 19:25:20 +0900

    45

--- Android/app/src/main/java/com/example/viewmodel/ui/main/MainFragment.kt
+++ Android/app/src/main/java/com/example/viewmodel/ui/main/MainFragment.kt
@@ -1,15 +1,23 @@
 package com.example.viewmodel.ui.main
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryOwner
 import com.example.viewmodel.R
+import com.example.viewmodel.databinding.FragmentMainBinding
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(){
 
+    private lateinit var binding: FragmentMainBinding
     companion object {
         fun newInstance() = MainFragment()
     }
@@ -18,18 +26,49 @@ class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         viewModel = ViewModelProvider(
             this,
-            ViewModelProvider.NewInstanceFactory()
+            ViewModelProvider.NewInstanceFactory() // TODO: ViewModel Factory! SavedStateViewModelFactory
         ).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        binding.lifecycleOwner = this
+
+        binding.vm = viewModel
+        binding.ui = this
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val observer = Observer<Float> { result ->
+            binding.tv.text = result.toString()
+        }
+        viewModel.result().observe(viewLifecycleOwner, observer)
+
+        binding.apply {
+            btn.setOnClickListener {
+                if (et.text.isNullOrEmpty()) {
+                    tv.text = "null"
+                } else {
+                    viewModel.set(et.text.toString())
+                    tv.text = viewModel.result().toString()
+                }
+            }
+
+        }
+
+
+
+
     }
 
 }
--- Android/app/src/main/java/com/example/viewmodel/ui/main/MainViewModel.kt
+++ Android/app/src/main/java/com/example/viewmodel/ui/main/MainViewModel.kt
@@ -1,7 +1,29 @@
 package com.example.viewmodel.ui.main
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.SavedStateHandle
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    private val rate = 0.8f
+    var dollar : MutableLiveData<String> = MutableLiveData()
+    var result : MutableLiveData<Float> = MutableLiveData()
+
+    fun set(value: String){
+        this.dollar.value = value
+        result.value = value.toFloat()* rate
+    }
+    fun result():MutableLiveData<Float>{
+        val test = "TEST"
+        savedStateHandle[test] = "SAVED"
+        return result
+    }
+
+    fun reload(){
+        var saveLive : LiveData<String> = savedStateHandle.getLiveData("TEST")
+        var save = savedStateHandle.get<String>("TEST")
+    }
+
+
 }
