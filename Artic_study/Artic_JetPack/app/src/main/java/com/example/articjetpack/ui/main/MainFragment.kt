77ef9aa63719b9e8e1ebd9a3d501620a52bc30ae
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2023-01-25 15:45:43 +0900

    Flow Codelabs

---
+++ Artic_study/Artic_JetPack/app/src/main/java/com/example/articjetpack/ui/main/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.example.articjetpack.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.articjetpack.R
+import com.example.articjetpack.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+
+
+    }
+
+
+}
