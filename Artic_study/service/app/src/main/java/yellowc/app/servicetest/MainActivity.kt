c3de8cd19089c97647dcad7c68de448345c6e963
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2023-04-12 20:12:36 +0900

    [INIT] dagger stydy start

--- Artic_study/service/app/src/main/java/yellowc/app/servicetest/MainActivity.kt
+++ Artic_study/service/app/src/main/java/yellowc/app/servicetest/MainActivity.kt
@@ -1,11 +1,14 @@
 package yellowc.app.servicetest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import yellowc.app.servicetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ Artic_study/service/app/src/main/java/yellowc/app/servicetest/MyJobIntentService.kt
@@ -0,0 +1,10 @@
+package yellowc.app.servicetest
+
+import android.content.Intent
+import androidx.core.app.JobIntentService
+
+class MyJobIntentService : JobIntentService() {
+    override fun onHandleWork(intent: Intent) {
+        TODO("Not yet implemented")
+    }
+}
